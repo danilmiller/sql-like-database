1406005a8002c292989ed09be9434d60969e28db
Danil Miller <danilmiller10@gmail.com>
2024-08-06 00:04:45 +0200

    add database.py

---
+++ database.py
@@ -0,0 +1,23 @@
+
+class table():
+    pass
+
+def create_table():
+    pass
+
+def delete_table():
+    pass
+
+def insert_into():
+    pass
+
+def delete():
+    pass
+
+def update():
+    pass
+
+
+if __name__ == __main__:
+    pass
+    
